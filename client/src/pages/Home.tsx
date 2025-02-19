72088220ebe8601457f04a7666c14e6c966b1d4b
osm78704 <39805896-osm78704@users.noreply.replit.com>
2025-02-19 11:58:50 +0000

    Agent query: I've removed the About section between case studies and contact section. Could you verify if the page flows smoothly between these sections now?
    Remove "About" section from homepage.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/62444df7-2d33-4cdb-9e49-3d60753fb1ca/81da9921-6a99-40fb-ba20-66d429ba4c56.jpg

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,7 +5,6 @@ import TechIndex from "@/components/home/TechIndex";
 import Statistics from "@/components/home/Statistics";
 import Testimonials from "@/components/home/Testimonials";
 import CaseStudies from "@/components/home/CaseStudies";
-import About from "@/components/home/About";
 import ContactSection from "@/components/home/ContactSection";
 
 export default function Home() {
@@ -18,7 +17,6 @@ export default function Home() {
       <Statistics />
       <Testimonials />
       <CaseStudies />
-      <About />
       <ContactSection />
     </>
   );
